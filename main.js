2613014aeeda6032fd1180a1fa5772cd626e15ef
Rohan-Naran <100029666+Rohan-Naran@users.noreply.github.com>
2022-06-06 14:24:20 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+console.log("My first console project");
+
+var fn="Rohan";
+console.log(fn);
+
+var ln="Naran";
+console.log(ln);
+
+var fulln=fn.concat(ln);
+console.log(fulln);
+
+var a=10;
+console.log(a);
+
+var b=5;
+console.log(b);
+
+console.log(a+b);
+
+console.log(a*b);
+
+console.log(a/b);
+
